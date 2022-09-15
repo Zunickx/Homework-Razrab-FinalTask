7138c08007066dc2e1da6c9de3eb47402194303f
Zunickx <Zunickx@gmail.com>
2022-09-15 19:17:51 +0300

    TaskSpec Вывели получившийся массив

--- TaskSpec/Program.cs
+++ TaskSpec/Program.cs
@@ -13,3 +13,15 @@ void Modified(string[] textArray, string[] modifiedArray)
         }
     }
 }
+
+void PrintArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Modified(textArray, modifiedArray);
+PrintArray(modifiedArray);
