91388db7cf4584705b95c5832086100429b595ff
Zunickx <Zunickx@gmail.com>
2022-09-15 19:12:44 +0300

    TaskSpec Пробежались по массиву, создав массив из строк длинной <=3

--- TaskSpec/Program.cs
+++ TaskSpec/Program.cs
@@ -1,2 +1,15 @@
 ﻿string[] textArray = new string[5] {"hello", "2", "world", ":-)", "!"};
 string[] modifiedArray = new string[textArray.Length];
+
+void Modified(string[] textArray, string[] modifiedArray)
+{
+    int count = 0;
+    for(int i = 0; i < textArray.Length; i++)
+    {
+        if(textArray[i].Length <= 3)
+        {
+            modifiedArray[count] = textArray[i];
+            count++;
+        }
+    }
+}
